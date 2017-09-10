0003bd4b031d0dd1520c3704b84f18aee19b2cc5
Sarah Schaller <sarcsch@aol.com>
2017-09-10 12:33:14 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -20,4 +20,25 @@ class InteractiveRecord
     column_names.compact
   end
 
+  def self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
+
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert})
+    VALUES (#{values_for_insert})"
+
+    DB[:conn].execute(sql)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+
+
 end
