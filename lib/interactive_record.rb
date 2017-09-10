65bcea53577d7ba9d36df669f45cb65e38243831
Sarah Schaller <sarcsch@aol.com>
2017-09-10 12:38:23 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -42,7 +42,7 @@ class InteractiveRecord
   def values_for_insert
     values = []
     self.class.column_names.each do |col_name|
-      values << "#{send(col_name)}" unless send(col_name).nil?
+      values << "#{send('col_name')}" unless send(col_name).nil?
     end
     binding.pry
     values.join(", ")
